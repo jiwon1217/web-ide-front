f50d16630555da0c232c34516c28723efe0d972e
JIWON KWAK <koagjw@gmail.com>
2020-08-21 01:18:44 +0900

    yarn add terminal-in-react

--- src/workspace/components/BottomPanel.jsx
+++ src/workspace/components/BottomPanel.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import { Terminal } from './';
-
+import Terminal from 'terminal-in-react';
 function BottomPanel(props) {
-  return <Terminal />;
+  return (
+    <Terminal
+      hideTopBar={true}
+      allowTabs={false}
+      promptSymbol={'$'}
+      prompt={'lime'}
+      color={'lime'}
+      backgroundColor={'black'}
+      startState={'maximised'}
+    />
+  );
 }
 
 export default BottomPanel;
